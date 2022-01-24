d3e9988b548e07892b4bfc815e1dcf556f0b0218
vmdev10 <mouraanav@gmail.com>
2022-01-23 21:08:58 -0300

    feat: added CheckingAccount class

---
+++ bank-maut/src/CheckingAccount.java
@@ -0,0 +1,21 @@
+package src;
+
+public class CheckingAccount {
+  private double overdraft;
+
+  public CheckingAccount(double overdraft) {
+    this.overdraft = overdraft;
+  }
+
+  public void deposit(double value) {
+
+  }
+
+  public void withdraw(double value) {
+
+  }
+
+  public void transfer(double value, Account fromAccount, Account toAccount) {
+
+  }
+}
