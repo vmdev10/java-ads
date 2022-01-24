75a60c2730698549e23574cba19a332ade2408ac
vmdev10 <mouraanav@gmail.com>
2022-01-23 21:10:37 -0300

    feat: added SavingsAccount class

---
+++ bank-maut/src/SavingsAccount.java
@@ -0,0 +1,13 @@
+package src;
+
+public class SavingsAccount {
+  private double income;
+
+  public SavingsAccount(double income) {
+    this.income = income;
+  }
+
+  public void transfer(double value, Account fromAccount, Account toAccount) {
+
+  }
+}
